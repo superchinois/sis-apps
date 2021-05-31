5582465c9f79d84637451a66ddf6d8281bdedb66
superchinois <eric.lichamyon@gmail.com>
2021-05-31 17:36:02 +0400

    modified to match current deployment

---
+++ src/routes/Stats.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+import ChartForm from './../components/ChartForm';
+
+const Stats = () => (
+  <Fragment>
+    <ChartForm></ChartForm>
+  </Fragment>
+)
+
+export default Stats
