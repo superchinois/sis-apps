f927118dd6d5c4cc08d51a9ae9f0e270bd2ee993
superchinois <eric.lichamyon@gmail.com>
2021-07-02 12:40:59 +0400

    added dao for item endpoint

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,6 +1,30 @@
 import React  from 'react';
 import TypeaheadRemote from '../components/TypeaheadRemote';
 import {zipObject} from 'lodash';
+import axios from 'axios';
+
+class DbDAO {
+    constructor(base_url){
+        this.base_url = base_url;
+        this.item_id_url = (path) => `${base_url}/${path}`;
+    }
+    get remote_url() {
+        return this.base_url;
+    }
+    getItem = (item_id) => axios({method:"get", url:this.item_id_url(item_id)});
+    fetchItems = (params) => axios({method:"get", url:this.remote_url, params:params});
+    createItemInDb = (createdItem) =>{
+        return axios({method:"post", url:`${this.remote_url}`, data:createdItem});
+    };
+    updateItemInDB = (item_id, updatedValues)=>{
+        let item_url=this.item_id_url(item_id);
+        return axios({method:"put", url: item_url, data:updatedValues});
+    };
+    deleteItemInDb = (item_id) => {
+        return axios({method:"delete", url:item_id_url(item_id)});
+    };
+
+}
 
 let common_helpers = {
     buildTypeAheadComponent : (itemsSearchEndpoint, handleSelected, selected)=>{
@@ -33,6 +57,7 @@ let common_helpers = {
             createdItem = Object.assign(zipObject(fields, values), createdItem);
         }
         return createdItem;
-    }
+    },
+    buildDao : (base_url) => new DbDAO(base_url)
 }
 export default common_helpers;
