2dc8eb5be2277723fa042b122f2a0033e1a1e609
superchinois <eric.lichamyon@gmail.com>
2022-07-21 15:18:32 +0400

    added component

---
+++ src/routes/HistoriqueClientCA.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react'
+import Breadcrumb from 'react-bootstrap/Breadcrumb'
+import SimpleForm from '../components/SimpleForm'
+import ConfigApi from "../config.json";
+
+/**
+ * 
+ *   remote_url_fn: the remote url for getting computed data
+ *   textPlaceholder: text placeholder for what text to enter to search
+ *   search_url : URL to use to search for the query
+ */
+const HistoriqueClientCA = (props) => (
+  <Fragment>
+    <Breadcrumb>
+      <Breadcrumb.Item href="/">{props.rootLabel}</Breadcrumb.Item>
+      <Breadcrumb.Item active>{props.h1Title}</Breadcrumb.Item>
+    </Breadcrumb>
+    <SimpleForm search_url={`${ConfigApi.API_URL}/clients?search=`}
+                textPlaceholder="Entrer un nom de client ..."
+                remote_url_fn={cardcode=>`${ConfigApi.API_URL}/historique/${cardcode}/stats`}></SimpleForm>
+  </Fragment>
+)
+export default HistoriqueClientCA
